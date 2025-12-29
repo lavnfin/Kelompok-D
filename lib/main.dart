e2242197992460936202a638ecd6a9847870439b
Ditto2024 <ditoputra111@gmail.com>
2025-12-29 22:43:43 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'firebase_options.dart';
-import 'theme/app_theme.dart';
-import 'home.dart';
+import 'splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -20,10 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.light(),
-      home: const HomePage(),
+      home: SplashScreen(),
     );
   }
 }
